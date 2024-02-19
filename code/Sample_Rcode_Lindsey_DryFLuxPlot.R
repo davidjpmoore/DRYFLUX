3be29755e099209ad4045a76ebb98aba709ed49d
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-02-19 13:27:11 -0700

    total avg code complete

--- code/Sample_Rcode_Lindsey_DryFLuxPlot.R
+++ code/Sample_Rcode_Lindsey_DryFLuxPlot.R
@@ -95,7 +95,7 @@ for (i in annual_files) {
   # Specify the output file name for the average raster
   output_file_for_avg <- file.path(pathtoDryFluxGPP_AvgOutputFolder, paste0("average_", year, ".tif"))
   
-  # Write the average raster to a new GeoTIFF file
+  # Write the avg raster to a new tif file
   writeRaster(avg_rast, filename = output_file_for_avg, overwrite = TRUE)
   
   # Just extra- printing a message to confirm create of each avg raster
@@ -105,40 +105,34 @@ for (i in annual_files) {
 
 
 #================================================================================
-# Creating total avg (avgeraging annual averages into one raster)
+# Creating total avg (avgeraging annual averages into one raster). Drylands mask applied to total avg
 
 pathtoDryFluxGPP_AvgOutputFolder <- "./data/DRYFLUX_Avg_Outputs"  # Folder to store outputs from the code below that averages monthly and yearly dryflux outputs
 pathtoDryFluxGPP_YearFolder <- "./data/DRYFLUX_Avg_Outputs/Annual_Averages"  # Folder with all annual folders
+pathtoDrylandMask <- "./data/spatial/Drylands_dataset_2007/drylands_UNCCD_CBD_july2014.shp"  # Dryland mask file - it says 2014 but it's used for all years
 
 # Making a list of folders in DF output folder. List should be as long as the number of years of annual data (currently 17)
 annual_files <- list.files(pathtoDryFluxGPP_YearFolder, full.names = TRUE, recursive = FALSE)
 
 # Loop through each year raster to create a single average for 2000-2016 and store it in the avg output folder
 for (i in annual_files) {
-  # Get year from each folder name
-  year <- basename(i)
   
-  # Get list of monthly rasters in folder for each year; create a list to store rasters for averaging 
-  monthly_rasters <- list.files(i, pattern = "average_DF_X\\d{4}[A-Za-z]{3}\\d{4}\\.tif$", full.names = TRUE)
-  rast_for_avg <- list()
-  
-  # Loop through each month in the annual folder
-  for (i in monthly_rasters) {
-    # Read data in each raster and add to list (rast_for_avg) created above
-    rast_data <- rast(i)
-    rast_for_avg[[length(rast_for_avg) + 1]] <- rast_data
-  }
+  #load raster data for each annual tif and store 
+  rast_data <- rast(i)
+  rast_for_avg[[length(rast_for_avg) + 1]] <- rast_data
   
   # Create raster stack and compute avg per pixel 
   stacked_rast <- rast(rast_for_avg)
   avg_rast <- mean(stacked_rast)
   
+  # Load in and apply dryland mask to total avg raster
+  dryland_mask <- vect(pathtoDrylandMask)
+  avg_rast_masked <- mask(avg_rast, dryland_mask)
+  
   # Specify the output file name for the average raster
-  output_file_for_avg <- file.path(pathtoDryFluxGPP_AvgOutputFolder, paste0("average_", year, ".tif"))
+  output_file_for_avg <- file.path(pathtoDryFluxGPP_AvgOutputFolder, "2000_2016_masked_total_average.tif")
   
-  # Write the average raster to a new GeoTIFF file
+  # Write the avg raster to a new tif file
   writeRaster(avg_rast, filename = output_file_for_avg, overwrite = TRUE)
-  
-  # Just extra- printing a message to confirm create of each avg raster
-  cat("Average raster for", year, "has been created.\n")
-  }
+}
+
